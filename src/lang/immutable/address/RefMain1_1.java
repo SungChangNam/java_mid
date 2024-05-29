7770321f2e75b635b33fb9df60f772aaaa535adf
unknown <a90521@daum.net>
2024-05-29 15:26:00 +0900

    시본형과참조향,공유 참조 사이드

---
+++ src/lang/immutable/address/RefMain1_1.java
@@ -0,0 +1,15 @@
+package lang.immutable.address;
+
+public class RefMain1_1 {
+    public static void main(String[] args) {
+        //참조형 변수는 하나의 인스턴스를 공유할 수있다.
+        Address a= new Address("서울");
+        Address b =a;
+        System.out.println("A= "+a);
+        System.out.println("b= "+b);
+
+        b.setValue("부산");
+        System.out.println("A= "+a);
+        System.out.println("b= "+b);
+    }
+}
