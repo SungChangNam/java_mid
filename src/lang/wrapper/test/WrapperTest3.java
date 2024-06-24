494696c1b691653553c69411fbc32bf5bc98e591
Sung_cn_2 <112399821+SungChangNam@users.noreply.github.com>
2024-06-24 22:03:54 +0900

    System class method

---
+++ src/lang/wrapper/test/WrapperTest3.java
@@ -0,0 +1,19 @@
+package lang.wrapper.test;
+
+public class WrapperTest3 {
+    public static void main(String[] args) {
+        String str="100";
+
+        //Sting -> Integer
+        Integer integer1 = Integer.valueOf(str);
+        System.out.println("integer = " + integer1);
+
+        //Integer ->int
+        int intValue= integer1.intValue();
+        System.out.println("intValue = " + intValue);
+        
+        //int -> Integer
+        Integer integer2= Integer.valueOf(intValue);
+        System.out.println("integer2 = " + integer2);
+    }
+}
