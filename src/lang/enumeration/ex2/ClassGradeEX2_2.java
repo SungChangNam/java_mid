d4d2aeb7bc9bea3d46c60bde77b3453014398a8b
Sung_cn_2 <112399821+SungChangNam@users.noreply.github.com>
2024-06-25 23:42:08 +0900

    Eunmeration EUNM Class

---
+++ src/lang/enumeration/ex2/ClassGradeEX2_2.java
@@ -0,0 +1,15 @@
+package lang.enumeration.ex2;
+
+public class ClassGradeEX2_2 {
+    public static void main(String[] args) {
+        int price =10000;
+
+        DiscountService discountService = new DiscountService();
+
+ /*
+        ClassGrade newClassGrade =new ClassGrade();
+        int result= discountService.discount(newClassGradeprice);
+        System.out.println("newClassGrade = " + result);
+ */
+    }
+}
